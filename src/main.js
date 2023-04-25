ddcc8c43f2fe7f86a65854648ae9b9478fcfa840
Simon <simon.mollerberg@outlook.com>
2023-04-25 10:24:24 +0200

    updates

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
 import './assets/main.css'
 const app = createApp(App)
-// import Toastify from 'vue3-toastify';
-// app.use(Toastify)
-
+/*
+import Toastify from 'vue3-toastify'
+app.use(Toastify, {
+    autoClose: 1000,
+    position: "bottom-center",
+    style: {
+          bottom: 20 + "%",
+        }
+} );
+*/
 app.mount('#app')
